d1671686449174134182892a6fc0e1bf73ee8aee
MauriLS <mauriciolillo122@gmail.com>
2025-12-03 23:59:29 -0300

    Services Calzado

---
+++ app/src/main/java/com/example/movilepopshoes/data/remote/model/RetrofitClient.kt
@@ -0,0 +1,34 @@
+package com.example.movilepopshoes.data.remote
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitClient {
+
+    private const val BASE_URL = "http://10.0.2.2:8080/api/"
+
+    val apiService: PopShoesApiService by lazy {
+
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(PopShoesApiService::class.java)
+    }
+}
